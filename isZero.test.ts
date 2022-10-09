1be0b27e56ddc364c02f4111c6012287f48a3444
sudachi0114 <jnevariste@gmail.com>
2022-10-09 19:02:01 +0900

    false が返ってくる場合のテストケースを追加

--- isZero.test.ts
+++ isZero.test.ts
@@ -4,3 +4,8 @@ test("isZero に 0 を渡すと true が返される", () => {
     const result = isZero(0);
     expect(result).toBe(true);
 });
+
+test("isZero に 3 を渡すと false が返される", () => {
+    const result = isZero(3);
+    expect(result).toBe(false);
+});
