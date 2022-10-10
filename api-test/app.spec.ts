471d12b27a01c335c95958334f065c07305209c8
sudachi0114 <jnevariste@gmail.com>
2022-10-10 16:40:37 +0900

    外部APIにリクエストするメソッドがあっても、テストではモックすると効率的

--- api-test/app.spec.ts
+++ api-test/app.spec.ts
@@ -1,5 +1,6 @@
-import app from "./app";
+import app, { requestAPI } from "./app";
 import request from "supertest";
+import axios from "axios";
 
 describe("express で立てたサーバーの動作テスト", () => {
     it("root (/) にリクエストするとレスポンスが返却される", async () => {
@@ -9,3 +10,19 @@ describe("express で立てたサーバーの動作テスト", () => {
         expect(response.body).toEqual({"status":"ok"});
     });
 });
+
+describe("外部と通信が発生するメソッドをモックしてテストする", () => {
+    let httpRequestMock: jest.SpyInstance;
+
+    beforeEach(() => {
+        httpRequestMock = jest.spyOn(axios, "get");
+        httpRequestMock.mockResolvedValue({ data: {message: "API response"}});
+    });
+
+    it("mocking したハンドラーでAPIからのレスポンスをテスト", async () => {
+        const response = await requestAPI();
+
+        expect(response.message).toBe("API response");
+        expect(httpRequestMock).toHaveBeenCalledWith("https://internal-api/get_data")
+    })
+})
--- api-test/app.ts
+++ api-test/app.ts
@@ -1,9 +1,16 @@
+import axios from "axios";
 import express from "express";
 
 const app = express();
 app.get("/", (request, response) => {
     response.json({"status": "ok"});
 });
+
+export async function requestAPI(): Promise<any> {
+    const response = await axios.get("https://internal-api/get_data"); // こんな内部APIは立てていないので、アクセスできない
+    return response.data;
+}
+
 
 app.listen(8081);
 
